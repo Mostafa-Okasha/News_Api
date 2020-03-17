640543b351a317be5e8f154462837e98c9b645cf
Mostafa Okasha <mostafa.okasha1999@gmail.com>
2020-03-17 18:59:32 +0200

    upload Project

--- js/index.js
+++ js/index.js
@@ -1,28 +1,20 @@
+/*import { get } from "http";*/
 
 let rowData = document.getElementById("rowData");
 let posts = "";
 let req;
+let category;
 let links = document.getElementsByClassName("nav-link");
-
-
 for(let i =0  ; i <links.length ; i++)
-    {
-        links[i].addEventListener("click", function(e){
-
-            let category  = e.target.innerHTML;
-            getNews(category);
-            
-        })
-    }
-
-
-getNews('technology');
-
+{
+    links[i].addEventListener("click", function(e){
+        category  = e.target.innerHTML;
+        getNews(category);
+    });
+}
+getNews("general");
 function getNews(category)
 {
-
-
-
         if(window.XMLHttpRequest)
             {
                 req = new XMLHttpRequest();
@@ -30,8 +22,9 @@ function getNews(category)
         else
             {
                 req = new ActiveXObject('Microsoft.Xmlhttp');
-            }    
-        let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=d34d49ce3a794aca80d1ae821239b0eb`
+            }
+            let url=`http://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=6aba64583ba8471e92ae0b53d6884d85`;
+    
         req.open('GET' , url );
 
         req.onreadystatechange =function()
@@ -43,37 +36,28 @@ function getNews(category)
                     posts = posts.articles;
                     displayData();
                 }
-
         }
 
         req.send();
 
 }
-
-
 function displayData()
 {
     let temp = ``;
 
     for(let i= 0 ;i<posts.length ; i++)
         {
-
-            temp +=` 
-            
-            <div class="col-md-3">
+            temp +=` <div class="col-md-4">
             <a href="${posts[i].url}">
                 <div class="post">
-                    <img class="img-fluid" src='${posts[i].urlToImage}' />
-                     <h3>${posts[i].title}</h3>       
-                     <p>post desc</p>
+                    <img class="img-fluid" src='${posts[i].urlToImage}'/>
+                    <h1>Author:</h1>
+                    <h3>${posts[i].author}.</h3>
+                     <h5>${posts[i].title}</h5>
+                     <p>${posts[i].content}</p>
                 </div> 
                 </a>
-            </div>
-        `
+            </div>`
         }
         rowData.innerHTML = temp;
 }
-
-
-
-
